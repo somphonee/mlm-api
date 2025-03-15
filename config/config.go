f9fa6bf3d891ff45968fadff53df6a379a761d59
spDev <sp.xayyalath@gmail.com>
2025-03-15 11:29:31 +0700

    [Add] confix LoadConfig getEnv getEnvAsInt

--- config/config.go
+++ config/config.go
@@ -1,1 +1,41 @@
 package config
+import (
+	"os"
+	"strconv"
+	"time"
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	ServerPort  string
+	DatabaseURL string
+	JWTSecret   string
+	JWTExpiry   time.Duration
+}
+
+func LoadConfig() *Config {
+	_ = godotenv.Load()
+	config := &Config{
+		ServerPort:  getEnv("SERVER_PORT", "8080"),
+		DatabaseURL: getEnv("DATABASE_URL", "postgres://postgres:postgres@localhost:5432/mlm_system"),
+		JWTSecret:   getEnv("JWT_SECRET", "your-secret-key"),
+		JWTExpiry:   time.Duration(getEnvAsInt("JWT_EXPIRY", 24)) * time.Hour,
+	}
+
+	return config
+}
+
+func getEnv(key, defaultValue string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return defaultValue
+}
+
+func getEnvAsInt(key string, defaultValue int) int {
+	valueStr := getEnv(key, "")
+	if value, err := strconv.Atoi(valueStr); err == nil {
+		return value
+	}
+	return defaultValue
+}
